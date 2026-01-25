a9e649aff298d907eb2336bfbf8e8967dd183961
“tmczs” <tmczs@proton.me>
2026-01-25 11:22:40 +0100

    robot.txt

--- viewis/urls.py
+++ viewis/urls.py
@@ -1,12 +1,13 @@
-# urls.py (główny)
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('', views.home, name='home'),
     path('en/', views.home, {'lang': 'en'}, name='home_en'),
     path('kontakt/', views.kontakt, name='kontakt'),
