47088bde75d9b3b042b0058f46edc2a7a81a4ddb
“tmczs” <tmczs@proton.me>
2026-02-09 00:37:48 +0100

    admin poprawa

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -5,21 +5,18 @@ class MediaInline(admin.TabularInline):
     model = Media
     extra = 0
     fields = ['file', 'order', 'media_type', 'hdr_setting']
+    readonly_fields = ['media_type']
     fk_name = 'parent_album'
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'media_type', 'order', 'created']
+    list_editable = ['order', 'media_type']
+    inlines = [MediaInline]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(parent_album__isnull=True)
 
 @admin.register(HDRSetting)
 class HDRSettingAdmin(admin.ModelAdmin):
     list_display = ['name', 'id']
-
-@admin.register(Media)
-class MediaAdmin(admin.ModelAdmin):
-    list_display = ['display_name', 'media_type', 'order', 'created']
-    list_editable = ['order']
-    inlines = [MediaInline]
-
-    def display_name(self, obj):
-        return f"📁 {obj.name}" if obj.media_type == 'album' else f"📄 {obj.file.name.split('/')[-1] if obj.file else obj.id}"
-    display_name.short_description = 'Element'
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).filter(parent_album__isnull=True)
--- portfolio/models.py
+++ portfolio/models.py
@@ -6,13 +6,18 @@ class HDRSetting(models.Model):
     def __str__(self): return self.name
 
 class Media(models.Model):
-    MEDIA_TYPES = [('image', 'Image'), ('video', 'Video'), ('model3d', 'Model 3D'), ('album', 'Album')]
+    TYPES = [
+        ('image', 'Image'),
+        ('video', 'Video'),
+        ('model3d', 'Model 3D'),
+        ('album', 'Album'),
+    ]
     
     name = models.CharField(max_length=200, blank=True)
     file = models.FileField(upload_to='media/', null=True, blank=True)
+    media_type = models.CharField(max_length=10, choices=TYPES, default='image')
     parent_album = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     hdr_setting = models.ForeignKey(HDRSetting, on_delete=models.SET_NULL, blank=True, null=True)
-    media_type = models.CharField(max_length=10, choices=MEDIA_TYPES, default='image')
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     order = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,9 +31,9 @@ class Media(models.Model):
             if ext in ['jpg', 'jpeg', 'png', 'gif', 'webp']: self.media_type = 'image'
             elif ext in ['mp4', 'webm', 'mov']: self.media_type = 'video'
             elif ext in ['glb', 'gltf']: self.media_type = 'model3d'
-        elif not self.parent_album:
+        elif not self.file and not self.parent_album:
             self.media_type = 'album'
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name if self.media_type == 'album' else f"{self.media_type} {self.id}"
+        return self.name if self.name else f"{self.media_type} {self.id}"
